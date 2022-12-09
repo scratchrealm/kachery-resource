6829e4e86f4ca871eff10329cfd000dbd1cb4f1a
Jeremy Magland <jmagland@flatironinstitute.org>
2022-12-09 14:41:11 -0500

    fix linked files

--- src/FileUploadJob.ts
+++ src/FileUploadJob.ts
@@ -154,8 +154,39 @@ const getKacheryFilePath = (uri: string): string => {
     return `${kacheryCloudDir}/sha1/${s[0]}${s[1]}/${s[2]}${s[3]}/${s[4]}${s[5]}/${s}`
 }
 
-const getKacheryFileInfo = async (uri: string): Promise<KacheryFileInfo> => {
+const getKacheryFileInfo = async (uri: string): Promise<KacheryFileInfo | undefined> => {
     const path = getKacheryFilePath(uri)
+    let stat: fs.Stats
+    try {
+        stat = await fs.promises.stat(path)
+    }
+    catch(err) {
+        return await getKacheryLinkFileInfo(uri)
+    }
+    return {
+        path,
+        size: stat.size
+    }
+}
+
+const getKacheryFileLinkPath = (uri: string): string => {
+    const a = uri.split('/')
+    const sha1 = a[2]
+    const kacheryCloudDir = process.env['KACHERY_CLOUD_DIR'] || `${os.homedir()}/.kachery-cloud`
+    const s = sha1
+    return `${kacheryCloudDir}/linked_files/sha1/${s[0]}${s[1]}/${s[2]}${s[3]}/${s[4]}${s[5]}/${s}`
+}
+
+const getKacheryLinkFileInfo = async (uri: string): Promise<KacheryFileInfo | undefined> => {
+    const linkPath = getKacheryFileLinkPath(uri)
+    if (!fs.existsSync(linkPath)) {
+        return undefined
+    }
+    const a = await fs.promises.readFile(linkPath, 'utf-8')
+    const {path, size, mtime} = JSON.parse(a)
+    if (!fs.existsSync(path)) {
+        return undefined
+    }
     let stat: fs.Stats
     try {
         stat = await fs.promises.stat(path)
@@ -163,11 +194,16 @@ const getKacheryFileInfo = async (uri: string): Promise<KacheryFileInfo> => {
     catch(err) {
         return undefined
     }
+    if (stat.size !== size) {
+        return undefined
+    }
+    // We won't check the mtime here, even though perhaps we should
     return {
         path,
         size: stat.size
     }
 }
+
 
 export const randomAlphaString = (num_chars: number) => {
     if (!num_chars) {
